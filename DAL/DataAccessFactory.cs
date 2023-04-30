8063c8d5a15ceac504f1fa266475d99b7c07b3af
Sayem Mustak Hasan <mustakhasansayem@gmail.com>
2023-05-01 03:18:06 +0600

    Add project files.

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<User,string,User> UserData()
+        {
+            return new UserRepo();
+        }
+
+        public static IRepo<Movie, int, bool> MovieData()
+        {
+            return new MovieRepo();
+        }
+    }
+}
---
+++ DAL/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    internal class Context: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
